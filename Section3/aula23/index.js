00a2429b1c8fbbd8cb40ee77cc6fdf568b62a14e
Pedro Oliveira <91897520+OnlyPedro@users.noreply.github.com>
2023-04-27 21:30:33 -0300

    add: Section 3

---
+++ Section3/aula23/index.js
@@ -0,0 +1,17 @@
+/*
+Operadores de Comparaçao
+> maior que
+>= maior que ou igual a
+< menor que
+<= menor que ou igual a
+== igualdade (valor)           //!(= atribuiçao)
+=== igualadade estrita (Valor e tipo)
+!= diferente (valor)
+!== diferente estrito (Valor e tipo)
+*/
+
+const num1 = 10;
+const num2 = 10;
+const comp = num1 !== num2;
+
+console.log(comp);
