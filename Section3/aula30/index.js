c684b97750a11debccb85d58e5598096c9b63f2c
Pedro Oliveira <pedrofo2002@gmail.com>
2023-12-02 14:22:02 -0300

    Update index.js

--- Section3/aula30/index.js
+++ Section3/aula30/index.js
@@ -1,1 +1,54 @@
-console.log('Olá');
+function getDiaSemanaTexto(diaSemana) {
+    let diaSemanaTexto;
+
+    switch (diaSemana) {
+        case 0:
+            diaSemanaTexto = 'Domingo';
+            return diaSemanaTexto;
+        case 1:
+            diaSemanaTexto = 'Segunda';
+            return diaSemanaTexto;
+        case 2:
+            diaSemanaTexto = 'Terça';
+            return diaSemanaTexto;
+        case 3:
+            diaSemanaTexto = 'Quarta';
+            return diaSemanaTexto;
+        case 4:
+            diaSemanaTexto = 'Quinta';
+            return diaSemanaTexto;
+        case 5:
+            diaSemanaTexto = 'Sexta';
+            return diaSemanaTexto;
+        case 6:
+            diaSemanaTexto = 'Sábado';
+            return diaSemanaTexto;
+        default:
+            diaSemanaTexto = '';
+            return diaSemanaTexto;
+    }
+}
+
+const data = new Date('1987-04-25 00:00:00');
+const diaSemana = data.getDay();
+const diaSemanaTexto = getDiaSemanaTexto(diaSemana);
+
+console.log(diaSemana, diaSemanaTexto);
+
+// if (diaSemana === 1) {
+//     diaSemanaTexto = 'Segunda';
+// } else if (diaSemana === 2) {
+//     diaSemanaTexto = 'Terça';
+// } else if (diaSemana === 3) {
+//     diaSemanaTexto = 'Quarta';
+// } else if (diaSemana === 4) {
+//     diaSemanaTexto = 'Quinta';
+// } else if (diaSemana === 5) {
+//     diaSemanaTexto = 'Sexta';
+// } else if (diaSemana === 6) {
+//     diaSemanaTexto = 'Sábado';
+// } else if (diaSemana === 0) {
+//     diaSemanaTexto = 'Domingo';
+// } else {
+//     diaSemanaTexto = '';
+// }
