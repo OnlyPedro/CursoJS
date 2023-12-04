bb22f72b52a5223861a328dd767a796078503d1b
Pedro Oliveira <pedrofo2002@gmail.com>
2023-12-03 23:20:30 -0300

    add: aula31

---
+++ Section3/aula31/index.js
@@ -0,0 +1,39 @@
+const verdadeira = true;
+
+// // Let tem escopo de bloco { ... bloco }
+// // Var só tem escopo de funçao
+
+// let nome = 'Luiz'; //criando
+// var nome2 = 'Luiz'; //criando
+
+// if (verdadeira) {
+//     let nome = 'Pedro'; // criando
+//     var nome2 = 'Rogerio'
+
+//     var nome2 = 'Pedro'; // Redeclarada
+//     console.log(nome, nome2);
+
+//     if (verdadeira) {
+//         let nome = 'Outra coisa';
+//         var nome2 = 'Outra coisa'; // Redeclarada
+//         console.log(nome, nome2);
+//     }
+// }
+
+// console.log(nome, nome2);
+
+// function falaOi () {
+    
+//     if (verdadeira) {
+//         let nome = 'Pedro';
+//         var sobrenome = 'Miranda';
+//     }
+    
+//     console.log(sobrenome);
+// }
+
+// falaOi();
+
+let sobrenome = 'Miranda';
+console.log(sobrenome);
+
